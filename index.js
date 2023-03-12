614e6ef2cc6ef9fb884f6ce4d14d85ecccce7d14
Jan Sevilla <blastersevilla@hotmail.com>
2023-03-12 16:11:26 -0400

    challenge 1 done

---
+++ index.js
@@ -0,0 +1,15 @@
+let btnC = document.querySelector('.btn');
+let textH = document.querySelector('.headerh1');
+
+btnC.addEventListener('click', changeText);
+
+let oText = textH.textContent;
+function changeText(){
+    textH.innerHTML = "Working, Please Wait...";
+    setTimeout(timerText, 5000);
+
+}
+
+function timerText(){
+    textH.innerHTML = oText;
+}
